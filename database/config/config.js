14edc5b2a0ef71872bc0b9a247b97b65c558b8fa
ligia05 <92107626+ligia05@users.noreply.github.com>
2022-02-24 12:12:28 -0300

    1

--- database/config/config.js
+++ database/config/config.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+require ('dotenv').config();
 
 module.exports = {
+
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT
-}
+    "dialect": process.env.DB_DIALECT,
+    "port":process.env.DB_PORT
+  }
